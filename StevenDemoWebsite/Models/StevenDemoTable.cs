15948c6753224afe90a4ab3b7208d98ec4fb75f2
stevenlai1688 <70926365+stevenlai1688@users.noreply.github.com>
2021-06-15 13:53:00 -0700

    Add files via upload

---
+++ StevenDemoWebsite/Models/StevenDemoTable.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace StevenDemoWebsite.Models
+{
+    public partial class StevenDemoTable
+    {
+        
+        public int Id { get; set; }
+        [DisplayName("Reason for Changes")]
+        public string RequestReason { get; set; }
+        [Required]
+        [DisplayName("Name")]
+        public string RequestorName { get; set; }
+
+        [DisplayName("Date")]
+        [DataType(DataType.Date)]
+        public DateTime DesiredCompletionDate { get; set; }
+        
+        [DisplayName("Priority")]
+        [Required]
+        public string PriorityLevel{ get; set;}
+
+        [DisplayName("Description of Changes")]
+        public string RequestDescription { get; set; }
+
+        [DisplayName("Changes Requested")]
+        public string RequestChanges { get; set; }
+
+
+        [DisplayName("Effect on Organization")]
+        public string RequestEffectsOnOrganization { get; set; }
+
+
+
+
+
+
+    }
+}
