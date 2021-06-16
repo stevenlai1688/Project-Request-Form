109228cf3e5b943cb76d4676960a0c7d294bffe1
stevenlai1688 <stevenlai1688@gmail.com>
2021-06-16 14:57:35 -0700

    Import Mapper and added configuration to help map between View Model and Entity Model

---
+++ StevenDemoWebsite/AutoMapperConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Project.Models;
+
+namespace Project.Request.Web
+{
+    public class AutoMapperConfiguration : Profile
+    {
+        public AutoMapperConfiguration()
+        { 
+
+            CreateMap<StevenDemoTableViewModel, StevenDemoTable>();
+            CreateMap<StevenDemoTable, StevenDemoTableViewModel>();
+            
+            CreateMap<PriorityLevelViewModel, PriorityLevel>();
+            CreateMap<PriorityLevel, PriorityLevelViewModel>();
+        }
+    }
+}
