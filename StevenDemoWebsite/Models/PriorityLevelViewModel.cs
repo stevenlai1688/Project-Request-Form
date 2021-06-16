f17c63ce1d49e333bd435617955cf9186cfbca8b
stevenlai1688 <stevenlai1688@gmail.com>
2021-06-15 17:05:36 -0700

    Created a new ViewModel to help filtering indexes based on priority levels

---
+++ StevenDemoWebsite/Models/PriorityLevelViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace StevenDemoWebsite.Models
+{
+        public class PriorityLevelViewModel
+        {
+            public List<StevenDemoTable> StevenTables { get; set; }
+            public SelectList Priority { get; set; }
+            public string SearchPriority { get; set; }
+            public string SearchName { get; set; }
+        }
+    
+}
