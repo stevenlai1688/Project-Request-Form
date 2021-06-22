692cb2beef39a57a6c5e60169cd04c28a603b601
stevenlai1688 <stevenlai1688@gmail.com>
2021-06-22 16:31:24 -0700

    Added DbContextFactory

---
+++ Project.Request.Data/Factory/DbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Project.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Project.Request.Web.Models;
+
+namespace Project.Request.Data.Factory
+{
+    public class DbContextFactory : IDbContextFactory
+    {
+        private readonly IOptions<ConnectionStrings> _connectionStrings;
+        public DbContextFactory(IOptions<ConnectionStrings> connectionStrings)
+        {
+            _connectionStrings = connectionStrings;
+        }
+        StevenDemoWebsiteContext IDbContextFactory.create()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<StevenDemoWebsiteContext>();
+            optionsBuilder.UseSqlServer(_connectionStrings.Value.OperationalConnection);
+            return new StevenDemoWebsiteContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ Project.Request.Data/Factory/IDbContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using Project.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Request.Data.Factory
+{
+    public interface IDbContextFactory
+    {
+        StevenDemoWebsiteContext create();
+    }
+}
