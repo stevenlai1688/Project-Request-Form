29167a036021aac837be5cd7dfe376158b509406
stevenlai1688 <stevenlai1688@gmail.com>
2021-06-23 16:41:09 -0700

    Removed [Required] Attribute for bug fixes

--- StevenDemoWebsite/Models/CostCenterViewModel.cs
+++ StevenDemoWebsite/Models/CostCenterViewModel.cs
@@ -21,5 +21,29 @@ namespace Project.Models
         [DisplayName("Modified Date")] 
         public DateTime? ModifiedDate { get; set; }
 
+        public string Capitalized
+        {
+            get
+            {
+                var capitalized = string.Empty;
+                if (string.IsNullOrEmpty(Description)) return string.Empty;
+
+                var arr = Description.Split(" ");
+                if (arr.Length == 1)
+                {
+                    capitalized = arr[0].Substring(0, 1).ToUpper() + arr[0].Substring(1).ToLower();
+                }
+                else {
+                    foreach (string item in arr)
+                    {
+                        string toBeCapitalized = item.Substring(0, 1).ToUpper() + item.Substring(1).ToLower();
+                        capitalized += string.IsNullOrEmpty(capitalized) ? toBeCapitalized : " " + toBeCapitalized;
+                    }
+
+                }
+                // 'It' is an exception, it should be 'IT'
+                return capitalized == "It" ? "IT" : capitalized;
+            }
+        }
     }
 }
